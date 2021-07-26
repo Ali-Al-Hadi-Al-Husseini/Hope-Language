a946cae29df6ae0c005a84fd28f42f52dd6ab8d6
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-07-26 13:35:10 +0300

    added command to exit fixed a small bug

--- shell.py
+++ shell.py
@@ -1,11 +1,16 @@
 import Hope
 
+ 
 
 while True:
     text = input('Hope >>>')
-    result, error = Hope.Run(text)
+    result, error = Hope.Run(text, '<stdin>')
+
+    if text == 'exit':
+        break
 
     if error:
         print(error.as_string())
+
     else:
         print(result)
