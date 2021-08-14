7d39f42fbab560fe9ab356aa88a43f483d78c484
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-08-15 00:00:24 +0300

    Update shell.py

--- shell.py
+++ shell.py
@@ -5,12 +5,13 @@ import Hope
 while True:
     text = input('Hope >>>')
     result, error = Hope.Run(text, '<stdin>')
-
+    
     if text == 'exit':
         break
 
     if error:
         print(error.as_string())
 
-    else:
+    elif result:
         print(result)
+    
