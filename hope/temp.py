d01094fea35a38b343c54bb178b101d228284193
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2022-10-28 22:02:32 +0300

    added test cases

--- hope/temp.py
+++ hope/temp.py
@@ -1,7 +1,5 @@
 from Hope import run,InvalidSyntaxErorr
 
 
-x ,err = run("""for i= 1 -> 5 >>
-                print(i) ;
-                <<      """,'hola.hope')
+x ,err = run("""let i = 0; while i < 10 >>; print(i); i = i + 1 ;<< ; print(i)""",'hola.hope')
 print(x,err)
