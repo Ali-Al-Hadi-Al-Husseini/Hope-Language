4349a1189e18e66d68e9fe563a480ec56057ed43
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-14 15:34:05 +0200

    changed import in hope/init

--- hope/__init__.py
+++ hope/__init__.py
@@ -1,9 +1,9 @@
-from Interpreter_tools.Interpreter import run,Function
-from Errors_tools.Errors import *
-from Interpreter_tools.Types import String,Number,List
-from Tokenizer_tools.Tokenizer import Tokenizer
-from Tokenizer_tools.Token import Token
-from Parser_tools.Parser import Parser
+from .Interpreter_tools.Interpreter import run,Function
+from .Errors_tools.Errors import *
+from .Interpreter_tools.Types import String,Number,List
+from .Tokenizer_tools.Tokenizer import Tokenizer
+from .Tokenizer_tools.Token import Token
+from .Parser_tools.Parser import Parser
 
 
 __all__ = ['Parser','Token','Tokenizer','run','IllegalCharError','InvalidSyntaxErorr','Indexerror','RunTimeError','ExpectedCharError','Function']  + ['List','String','Number']
