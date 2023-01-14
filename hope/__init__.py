ee46d35f5e17e4a1c78b5f9dbfd269e16df39736
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-14 13:28:20 +0200

    added parser for imports

--- hope/__init__.py
+++ hope/__init__.py
@@ -3,6 +3,7 @@ from Errors_tools.Errors import *
 from Interpreter_tools.Types import String,Number,List
 from Tokenizer_tools.Tokenizer import Tokenizer
 from Tokenizer_tools.Token import Token
+from Parser_tools.Parser import Parser
 
 
-__all__ = ['Token','Tokenizer','run','IllegalCharError','InvalidSyntaxErorr','Indexerror','RunTimeError','ExpectedCharError','Function']  + ['List','String','Number']
+__all__ = ['Parser','Token','Tokenizer','run','IllegalCharError','InvalidSyntaxErorr','Indexerror','RunTimeError','ExpectedCharError','Function']  + ['List','String','Number']
