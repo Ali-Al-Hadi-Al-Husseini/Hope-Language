f2a3dd0a86e114508fa8e41e594bf7996ae09d9f
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-09-03 11:39:39 +0300

    added postion methods

--- hope-go/src/lexer/token.go
+++ hope-go/src/lexer/token.go
@@ -16,3 +16,17 @@ type CodeFile struct {
 	Name string
 	Text string
 }
+
+func (pos *Position) Advance(currChar string) Position {
+	pos.Idx += 1
+	pos.Col += 1
+
+	if currChar == "\n" {
+		pos.Line += 1
+		pos.Col += 1
+	}
+	return *pos
+}
+func (pos Position) Copy() Position {
+	return pos
+}
