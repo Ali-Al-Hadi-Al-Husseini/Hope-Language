16c773345535f0b5bfd532cb904f525c7c28dced
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-09-06 13:43:42 +0300

    added more make funcs and more cases

--- hope-go/src/lexer/lexer.go
+++ hope-go/src/lexer/lexer.go
@@ -1,12 +1,14 @@
 package lexer
 
+import "fmt"
+
 type Lexer struct {
 	CurrChar byte
 	Pos      *Position
 	File     *CodeFile
 }
 
-func (lexer *Lexer) lexer(code string) []Token {
+func (lexer *Lexer) lexer(code string) ([]Token, error) {
 	tokens := []Token{}
 
 	for lexer.CurrChar != 0 {
@@ -23,14 +25,43 @@ func (lexer *Lexer) lexer(code string) []Token {
 		case ';', '\n':
 			lexer.makeNewLine()
 
+		case '"', '\'':
+			lexer.makeStr()
+
+		case '+', '-', '/', '*', '%', '^':
+			lexer.makeOperationAndEqual()
+
+		case '!':
+			lexer.makeNotEqual()
+
+		case '&', '|':
+			lexer.makeLogicalGate()
+
+		case '{', '}', '[', ']', '(', ')':
+			// add token here
+			lexer.advance(true)
+
+		case '=':
+			lexer.makeEqual()
+
+		case '>', '<':
+			lexer.makeGtLt()
+
 		default:
 			switch {
 			case isdigit(lexer.CurrChar):
 				lexer.makeNumber()
+
+			case isLetter(lexer.CurrChar):
+				lexer.makeIdentifier()
+
+			default:
+				return []Token{}, fmt.Errorf("IllegalChar")
 			}
+
 		}
 	}
-	return tokens
+	return tokens, nil
 }
 
 func (lexer *Lexer) advance(advanceChar bool) {
@@ -66,6 +97,20 @@ func (Lexer *Lexer) makeGtLt() {
 func (Lexer *Lexer) makeEqual() {
 
 }
+func (Lexer *Lexer) makeNotEqual() {
+
+}
+func (Lexer *Lexer) makeIdentifier() {
+
+}
+func (Lexer *Lexer) makeLogicalGate() {
+
+}
+
 func isdigit(ch byte) bool {
 	return '0' <= ch && '9' >= ch
+}
+func isLetter(ch byte) bool {
+
+	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z')
 }
