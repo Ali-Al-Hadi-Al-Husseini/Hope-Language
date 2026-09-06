22bf605b098f4d7a103f12362fb74cc2e21eb361
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-09-06 11:40:12 +0300

    added advance func

--- hope-go/src/lexer/lexer.go
+++ hope-go/src/lexer/lexer.go
@@ -1,10 +1,24 @@
 package lexer
 
 type Lexer struct {
-	CodeFile
-	Position
+	CurrChar string
+	Pos      *Position
+	File     *CodeFile
 }
 
 func (lexer *Lexer) lexer(code string) []string {
 	return []string{}
+}
+
+func (lexer *Lexer) advance(advanceChar bool) {
+	lexer.Pos.Advance(lexer.CurrChar)
+	if !advanceChar {
+		return
+	}
+	if lexer.Pos.Idx < len(lexer.File.Text) {
+		lexer.CurrChar = string(lexer.File.Text[lexer.Pos.Idx])
+
+	} else {
+		lexer.CurrChar = ""
+	}
 }
