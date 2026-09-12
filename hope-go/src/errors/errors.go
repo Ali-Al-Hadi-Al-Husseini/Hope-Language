c113110eb1ec16cc6cbce2c9fa1f1c3d79d6ae20
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-09-12 08:51:51 +0300

    created error struct

--- hope-go/src/errors/errors.go
+++ hope-go/src/errors/errors.go
@@ -1,1 +1,12 @@
 package errors
+
+import (
+	lexer "github.com/Ali-Al-Hadi-Al-Husseini/Hope-Language/hope-go/src/lexer"
+)
+
+type Error struct {
+	Name     string
+	Details  string
+	StartPos *lexer.Position
+	EndPos   *lexer.Position
+}
