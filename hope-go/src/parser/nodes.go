50f8cccd9ac1eb59493ce97dc9d4c7755147ee7d
Ali Al  Hadi Al Husseini <alihadi.alhusseini@gmail.com>
2026-09-17 21:30:59 +0300

    added string method to base node and added strinnode struct

--- hope-go/src/parser/nodes.go
+++ hope-go/src/parser/nodes.go
@@ -1,12 +1,23 @@
 package parser
 
 import (
+	"fmt"
+
 	lexer "github.com/Ali-Al-Hadi-Al-Husseini/Hope-Language/hope-go/src/lexer"
 )
 
 type BaseNode struct {
+	Name     string
 	Token    *lexer.Token
 	Tokens   []*lexer.Token
 	StartPos *lexer.Position
 	EndPos   *lexer.Position
+}
+
+func (node *BaseNode) String() string {
+	return fmt.Sprintf("%v__%v", node.Token, node.Name)
+}
+
+type StringNode struct {
+	BaseNode
 }
